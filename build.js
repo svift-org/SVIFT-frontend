7e0d8f7c12f7b531183d269834b2552433404e05
sebastian-meier <contact@sebastianmeier.eu>
2017-08-02 17:25:39 +0200

    better build and test structure

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
 var UglifyJS = require('uglify-js2'),
+  sass = require('node-sass'),
   fs = require('fs'),
   package = require('./package.json')
 
@@ -8,29 +9,39 @@ var base = [
   './node_modules/svift-vis/index.js'
 ]
 
-var visualisations = ['all'], names = [null]
+var template = fs.readFileSync('tests/template.html', 'utf8')
+
+var visualisations = ['all']
 
 for(var key in package.dependencies){
   if(key.substring(0,10) == 'svift-vis-'){
-    visualisations.push('./node_modules/'+key+'/index.js')
-    names.push(key.substring(10))
+    visualisations.push(key)
   }
 }
 
+'./node_modules/'+key+'/index.js'
+
 function buildFile(vi){
-  var files = [],
+  var files = [], style = '',
     name = ''
-  base.forEach(function(b){files.push(b)})
+  base.forEach(function(b){
+    if(b.indexOf('svift-vis')>=0){
+      style += fs.readFileSync(__dirname + '/node_modules/svift-vis/style.scss', 'utf8')
+    }
+    files.push(b)
+  })
 
   if(visualisations[vi] == 'all'){
     visualisations.forEach(function(v){
       if(v!='all'){
-        files.push(v)    
+        files.push(__dirname + '/node_modules/'+v+'/index.js')
+        style += fs.readFileSync(__dirname + '/node_modules/'+v+'/style.scss', 'utf8')
       }
     })
   }else{
-    files.push(visualisations[vi])
-    name = '-' + names[vi]
+    files.push(__dirname + '/node_modules/'+visualisations[vi]+'/index.js')
+    style += fs.readFileSync('./node_modules/'+visualisations[vi] +'/style.scss', 'utf8')
+    name = '-' + visualisations[vi].substring(10)
   }
 
   var min = UglifyJS.minify(files, {
@@ -38,19 +49,26 @@ function buildFile(vi){
     sourceRoot: "http://svift.xyz/src"
   })
 
-  fs.writeFile("./build/svift" + name + ".min.js", min.code, function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      vi++
-      if(vi<visualisations.length){
-        buildFile(vi)
-      }else{
-        console.log('build done')
-        process.exit()
-      }
-    }
-  })
+  fs.writeFileSync(__dirname + "/build/svift" + name + ".min.js", min.code, 'utf8');
+
+  fs.writeFileSync(__dirname + "/build/svift" + name + ".css", sass.renderSync({
+      data:style,
+      outputStyle: 'compressed' 
+    }).css,
+    'utf8'
+  )
+
+  if(visualisations[vi]!='all'){
+    fs.writeFileSync(__dirname + '/tests/'+name.substring(1)+'.html', template.split('{{visname}}').join(name), 'utf8')
+  }
+
+  vi++
+  if(vi<visualisations.length){
+    buildFile(vi)
+  }else{
+    console.log('build done')
+    process.exit()
+  }
 }
 
 buildFile(0)
